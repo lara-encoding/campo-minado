48982e27a0531947b57daa42a961376c83c933d5
lara.encoding <analara.cafe@gmail.com>
2024-08-21 18:41:12 +0100

    implementando o estudo Correção na Distribuição das Minas.

--- src/br/com/cod3r/cm/visao/TabuleiroConsole.java
+++ src/br/com/cod3r/cm/visao/TabuleiroConsole.java
@@ -64,6 +64,7 @@ public class TabuleiroConsole {
 					
 			}
 			
+			System.out.println(tabuleiro);
 			System.out.println("Você ganhou!!!!");
 		} catch(ExplosaoException e) {
 			System.out.println(tabuleiro);
